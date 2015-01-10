f8371fc25c7b29ef3de26d75bcdc9adcbd119388
kaxla <kaylas.email.address@gmail.com>
2015-01-09 17:55:32 -0800

    add big picture consulting hack

---
+++ bigpictureconsulting.js
@@ -0,0 +1,38 @@
+<script type="text/javascript">
+  $(document).ready(function(){
+    $( "#header" ).prepend( "<div id='phonebar'><div id='phonebar-inner'><a href='mailto:contact@thinkingbigpicture.com'><b>Email Us</b></a> <div id='phone'>\(800\) 595-0438</div></div></div>" );
+  });
+</script>
+
+<style>
+#phonebar{
+  background-color: #283891;
+  padding: 10px;
+  width: 100%;
+  overflow:auto;
+}
+
+#phone{
+  padding-left: 15px;
+  display: inline;
+}
+
+#phonebar-inner{
+  float: right;
+  padding-right: 6%;
+  color: white;
+  text-transform: uppercase;
+}
+
+#phonebar-inner a:link{
+  color: white;
+}
+
+#phonebar-inner a:visited{
+  color: white;
+}
+
+#phonebar-inner a:hover{
+  color: #E0E0E1;
+}
+</style>
---
+++ change_link_location.js
@@ -0,0 +1,10 @@
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script type="text/javascript">
+
+$(document).ready(function(){
+
+  $("SELECTOR OF LINK").attr("href", "WHERE YOU WANT LINK TO GO TO NOW");
+
+});
+
+</script>
